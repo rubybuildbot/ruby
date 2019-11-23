cdb58d78334682d377315d4e484dff4d33ed1d6a
Renzo Crisostomo <renzo@crisostomo.me>
2019-11-23 22:35:49 +0100

    WIP

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -9,7 +9,7 @@ Read command, an additional delay of circa 31000 cycles occurs before IRQ7 gets 
 (that strange extra delay occurs only on original Sony cards, not on cards from other
 manufacturers).
 */
-const uint32_t SystemClocksPerControllerInt7 = 1500;
+const uint32_t SystemClocksPerControllerInt7 = 500;
 
 Controller::Controller(LogLevel logLevel, std::unique_ptr<InterruptController> &interruptController) : logger(logLevel, "  CONTROLLER: "), interruptController(interruptController), digitalController(make_unique<DigitalController>(logLevel)), currentDevice(NoDevice), control(), joypadBaud(), mode(), rxData(), status(), txData() {
 
@@ -42,27 +42,16 @@ void Controller::setTxDataRegister(uint8_t value) {
     txData._value = value;
     status.rxFifoNotEmpty = true;
 
-    if (currentDevice == NoDevice) {
-        if (value == Device::ControllerDevice) {
-            currentDevice = Device::ControllerDevice;
-        } else if (value == Device::MemoryCardDevice) {
-            currentDevice = Device::MemoryCardDevice;
-        }
-    }
-
-    if (currentDevice == Device::MemoryCardDevice) {
-        // TODO: Implement memory card
-        rxData.receivedData = 0xff;
-        return;
-    } else if (currentDevice == Device::ControllerDevice) {
+    if (control.joyOutput) {
         if (control.desiredSlotNumber == 1) {
             // TODO: Implement controller 2
             rxData.receivedData = 0xff;
             return;
         } else {
             rxData.receivedData = digitalController->getResponse(value);
-            status.ackInputLevel = digitalController->getAcknowledge();
-            if (status.ackInputLevel) {
+            control.acknowledge = digitalController->getAcknowledge();
+            status.ackInputLevel = true;
+            if (control.acknowledge) {
                 shouldCount = true;
                 counter = 0;
             }
@@ -80,9 +69,9 @@ uint8_t Controller::getRxDataRegister() {
 }
 
 uint32_t Controller::getStatusRegister() {
+    status.txReadyFlag1 = true;
+    status.txReadyFlag2 = true;
     logger.logMessage("JOY_STAT [R]: %#x", status._value);
-    status.txReadyFlag1 = true;
-    status.txReadyFlag1 = true;
     uint32_t value = status._value;
     status.ackInputLevel = false;
     return value;
